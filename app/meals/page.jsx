c9cccf8019a05765154a9b6d4b939d272f848832
devanshu <devanshu8220@gmail.com>
2025-03-25 13:08:20 +0530

    first commit

---
+++ app/meals/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Meals() {
+  return (
+    <div>meals</div>
+  )
+}
+
+export default Meals
---
+++ app/meals/share/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Share() {
+  return (
+    <div>share</div>
+  )
+}
+
+export default Share
